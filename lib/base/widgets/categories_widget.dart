b35b46db366f391d66a1e2f6e7718168adb9d8ca
Gabriela Prado <pradog19@gmail.com>
2025-04-01 15:21:16 +0100

    before adding grid

--- lib/base/widgets/categories_widget.dart
+++ lib/base/widgets/categories_widget.dart
@@ -18,43 +18,81 @@ Future<List> getFilteredItems(String category) async {
   return filteredItems;
 }
 
-int _initialIndex = 0;
+int _selectedIndex = 0;
+int _maxIndex = AppCategories().categoriesDB.length - 1;
 
 class _CategoriesWidgetState extends State<CategoriesWidget> {
+  void _onCategorySwipped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 30,
-          color: Colors.yellow,
-          child: Text(AppCategories().categoriesDB[0]),
-        ),
-        Container(
-          width: 200,
-          height: 500,
-          color: Colors.orange,
-          //Scrollable list of items on vertical
+    //Swipe rigth and left widget
+    return GestureDetector(
+      onHorizontalDragEnd: (details) {
+        int currentIndex = _selectedIndex;
+        if (details.primaryVelocity! > 0) {
+          currentIndex++;
+          if (currentIndex >= _maxIndex) {
+            _onCategorySwipped(_maxIndex);
+          } else {
+            _onCategorySwipped(currentIndex);
+          }
 
-          child: FutureBuilder<List<dynamic>>(
-            future: getFilteredItems(
-                'bathroom'), // Provide your asynchronous method here
-            builder:
-                (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator(); // Show a loading indicator while waiting
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return Text('No items available');
-              } else {
-                // Display the first item's string representation
-                return Text(snapshot.data![0].type.toString());
-              }
-            },
+          if (kDebugMode) {
+            print('Swiped right $_selectedIndex');
+          }
+          //checking max number of categories
+        } else if (details.primaryVelocity! < 0) {
+          currentIndex--;
+          //checking if is less than zero
+          if (currentIndex <= 0) {
+            _onCategorySwipped(0);
+          } else {
+            _onCategorySwipped(currentIndex);
+          }
+
+          if (kDebugMode) {
+            print('Swiped left $_selectedIndex');
+          }
+        }
+      },
+      child: Column(
+        children: [
+          Container(
+            height: 30,
+            color: Colors.yellow,
+            child: Text(AppCategories().categoriesDB[_selectedIndex]),
           ),
-        ),
-      ],
+          Container(
+            width: 200,
+            height: 500,
+            color: Colors.orange,
+            //Scrollable list of items on vertical
+
+            child: FutureBuilder<List<dynamic>>(
+              future: getFilteredItems(AppCategories().categoriesDB[
+                  _selectedIndex]), // Provide your asynchronous method here
+              builder: (BuildContext context,
+                  AsyncSnapshot<List<dynamic>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator(); // Show a loading indicator while waiting
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return Text('No items available');
+                } else {
+                  // Display the first item's string representation
+                  return Text(snapshot.data![0].type.toString());
+                }
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
